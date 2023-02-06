0a7eda80026ff36011e43b9c46e40598823b0702
Srinivas Joshi <srinivasjoshi60@gmail.com>
2023-02-06 11:55:27 +0530

    tailwind styling

---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<div className='flex justify-between items-center'>
+			<img src='' alt='Logo' />
+			//Wallet
+		</div>
+	);
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+
+		// Or if using `src` directory:
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				purple: '#5524e7',
+				yellow: '#fec231',
+				black: '#010101',
+			},
+			fontFamily: {
+				sans: ['Poppins, sans-serif'],
+				serif: ['Raleway,serfif'],
+			},
+		},
+	},
+	plugins: [],
+};
