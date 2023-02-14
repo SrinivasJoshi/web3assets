f080114b8d424665eea105c36e9788cb235d4ae6
soumalya.p <54816468+soumalya340@users.noreply.github.com>
2023-02-14 23:07:44 +0530

    Updated Test,web3Assets

--- contracts/web3Assets.sol
+++ contracts/web3Assets.sol
@@ -2,111 +2,165 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
 
-contract web3Assets{
-    IERC20 private _USDCtoken;
-    address payable private owner;
-    struct Asset{
-        uint256 assetId;
+contract web3Assets {
+    address public immutable I_Owner;
+    uint256 public totalAssets;
+
+    uint256 public platformfeeBasisPoint;
+
+    //to get the Interface of USD Tokens
+    IERC20 public _USDCtoken;
+
+    struct Asset {
+        string name;
         uint256 price;
         string link;
         address payable creator;
     }
-    uint256 public totalAssets;
     mapping(uint256 => Asset) assetList;
+
     mapping(uint256 => mapping(address => bool)) private isUserOwned;
 
-    event buyAssetEvent(uint256 _assetId,Asset _asset);
-    event createAssetEvent(uint256 _assetId,Asset _asset);
+    modifier onlyWhenItemExist(uint tokenId) {
+        require(tokenId <= totalAssets, "AssetShare: Token ID does not exist");
+        _;
+    }
+    modifier onlyWhenUserAlreadyNotBought(uint tokenId) {
+        require(
+            doesUserOwnAsset(tokenId) == false,
+            "AssetShare: User Already Bought"
+        );
+        _;
+    }
 
-    constructor(address _USDCtokenAddr){
-        owner = payable(msg.sender);
+    event AssetCreated(uint256 assetID, Asset asset);
+    event AssetSold(uint256 assetID, Asset asset);
+    event PriceUpdated(uint256 assetID, uint256 Price);
+
+    constructor(address _USDCtokenAddr, uint256 _platformfeeBasisPoint) {
         _USDCtoken = IERC20(_USDCtokenAddr);
+        I_Owner = msg.sender;
+        platformfeeBasisPoint = _platformfeeBasisPoint;
     }
 
-    function doesUserOwnAsset(uint256 _assetId) public view returns(bool){
-        return isUserOwned[_assetId][msg.sender];
+    function createAsset(
+        string memory _assetName,
+        string calldata _link,
+        uint256 _price
+    ) public {
+        totalAssets++;
+        uint tokenId = totalAssets;
+        assetList[tokenId] = Asset(
+            _assetName,
+            _price,
+            _link,
+            payable(msg.sender)
+        );
+        emit AssetCreated(tokenId, assetList[tokenId]);
     }
 
-    function buyAsset(uint256 _assetId) public payable{
-        require(_assetId < totalAssets,"Asset ID does not exist");
-        //add USDC code below
-        require(msg.value >= assetList[_assetId].price,"Not enough money to buy asset");
-        isUserOwned[_assetId][msg.sender] = true;
-        emit buyAssetEvent(_assetId,assetList[_assetId]);
+    function buyAssetInToken(
+        uint256 _tokenID
+    )
+        external
+        onlyWhenItemExist(_tokenID)
+        onlyWhenUserAlreadyNotBought(_tokenID)
+    {
+        uint tokenBalance = _USDCtoken.balanceOf(msg.sender);
+        require(
+            tokenBalance >= assetList[_tokenID].price,
+            "AssetShare: Not enough money to buy asset"
+        );
+        uint _price = assetList[_tokenID].price;
+        isUserOwned[_tokenID][msg.sender] = true;
 
-        //add USDC code below
-        assetList[_assetId].creator.transfer(msg.value * 90/100);
+        //payment for the Marketplace
+        uint256 payoutForMarketplace = (_price * platformfeeBasisPoint) / 1000;
+        //payment for payout
+        uint256 payoutForCreator = _price - payoutForMarketplace;
+        //Payout for marketplace
+        bool amountSent = _USDCtoken.transferFrom(
+            msg.sender,
+            I_Owner,
+            payoutForMarketplace
+        );
+        require(amountSent, "AssetShare: Transfer Failed !");
+        //Payout for creator
+        amountSent = _USDCtoken.transferFrom(
+            msg.sender,
+            assetList[_tokenID].creator,
+            payoutForCreator
+        );
+        require(amountSent, "AssetShare: Transfer Failed !");
+
+        emit AssetSold(_tokenID, assetList[_tokenID]);
     }
 
-    function createAsset(string calldata _link,uint256 _price) public{
-        Asset memory _asset = Asset({
-            assetId : totalAssets,
-            price : _price,
-            link:_link,
-            creator : payable(msg.sender)
-        });
-        assetList[totalAssets] = _asset;
-        emit createAssetEvent(totalAssets,_asset);
-        totalAssets+=1;
+    function doesUserOwnAsset(
+        uint256 tokenID
+    ) public view onlyWhenItemExist(tokenID) returns (bool) {
+        return isUserOwned[tokenID][msg.sender];
     }
 
-    function getMarketItems() public view returns(Asset[] memory){
+    function getMarketItems() external view returns (Asset[] memory) {
         Asset[] memory marketItems = new Asset[](totalAssets);
 
-        for(uint256 i;i < totalAssets;i++){
-            Asset storage currentItem = assetList[i];
-            marketItems[i] = currentItem;
+        for (uint256 i = 1; i <= totalAssets; i++) {
+            marketItems[i] = assetList[i];
         }
         return marketItems;
     }
 
-    function getCreatedAssets() public view returns(Asset[] memory){
-        uint256 itemCount = 0;
+    function getCreatedAssets() external view returns (Asset[] memory) {
+        uint256 itemCount = 1;
         uint256 currentIndex = 0;
 
-        for(uint256 i;i<totalAssets;i++){
-            if(assetList[i].creator == msg.sender){
+        for (uint256 i = 1; i < totalAssets; i++) {
+            if (assetList[i].creator == msg.sender) {
                 itemCount++;
             }
         }
 
         Asset[] memory createdAssets = new Asset[](itemCount);
 
-        for(uint256 i;i<totalAssets;i++){
-            if(assetList[i].creator == msg.sender){
-                Asset storage currentItem = assetList[i];
-                createdAssets[currentIndex] = currentItem;
-                currentIndex += 1;
+        for (uint256 i = 1; i <= totalAssets; i++) {
+            if (assetList[i].creator == msg.sender) {
+                createdAssets[currentIndex] = assetList[i];
+                currentIndex++;
             }
         }
         return createdAssets;
     }
 
-    function getBoughtAssets() public view returns(Asset[] memory){
+    function getBoughtAssets() external view returns (Asset[] memory) {
         uint256 itemCount = 0;
         uint256 currentIndex = 0;
 
-        for(uint256 i;i<totalAssets;i++){
-            if(isUserOwned[i][msg.sender]){
+        for (uint256 i = 1; i <= totalAssets; i++) {
+            if (isUserOwned[i][msg.sender]) {
                 itemCount++;
             }
         }
 
         Asset[] memory boughtAssets = new Asset[](itemCount);
 
-        for(uint256 i;i<totalAssets;i++){
-            if(isUserOwned[i][msg.sender]){
-                Asset storage currentItem = assetList[i];
-                boughtAssets[currentIndex] = currentItem;
+        for (uint256 i = 1; i <= totalAssets; i++) {
+            if (isUserOwned[i][msg.sender]) {
+                boughtAssets[currentIndex] = assetList[i];
                 currentIndex += 1;
             }
         }
         return boughtAssets;
     }
 
-    function withdraw() external{
-        require(msg.sender == owner,"Not the owner");
-        (owner).transfer(address(this).balance);
+    function updatePrice(uint tokenId, uint _price) external {
+        require(
+            msg.sender == assetList[tokenId].creator,
+            "AssetShare: You are not the Owner"
+        );
+        assetList[tokenId].price = _price;
+        emit PriceUpdated(tokenId, _price);
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 80001,
+    },
+  },
+  etherscan: {
+    apiKey: API_KEY,
+  },
 };
